1d227cf556ba1bb1900539c4d5b838e41b124470
dewaang-engminds <Dewaang.m@engineersmind.com>
2026-05-25 16:58:58 +0530

    chore/repo: untrack legacy pdf_autofillr_mapper-1.0.9 from git, add to .gitignore

--- packages/mapper/pdf_autofillr_mapper-1.0.9/src/pdf_autofillr_mapper/config/__init__.py
+++
@@ -1,2 +0,0 @@
-from pdf_autofillr_mapper.config.mapper_config import MapperConfig
-__all__ = ["MapperConfig"]
--- packages/mapper/pdf_autofillr_mapper-1.0.9/src/pdf_autofillr_mapper/groupers/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Grouper modules for form field organization.
-"""
-from .base_grouper import BaseGrouper
-from .group_by_llm import GroupByLLM
-
-__all__ = ["BaseGrouper", "GroupByLLM"]
